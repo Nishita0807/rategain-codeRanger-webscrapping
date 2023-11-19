2280a6076d172b6777e1b1a36f1b851e76d28b47
Nishita0807 <saranj0903@gmail.com>
2023-11-19 09:26:22 +0530

    final commit

--- main.py
+++ main.py
@@ -4,25 +4,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
+import subprocess  # Import the subprocess module
 
 url = "https://rategain.com/blog/"
 
 # Set up the web driver in headless mode
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+options.add_argument('--blink-settings=imagesEnabled=false')  # Disable image loading
 driver = webdriver.Chrome(options=options)
 
 try:
     # Initialize an empty list to store data
     blog_data = []
 
+    # Record the start time
+    start_time = time.time()
+
     # Iterate through pagination pages
     page_num = 1
     while True:
         driver.get(url + f"page/{page_num}/")
 
-        # Wait for the content to load (reduce the timeout as needed)
-        WebDriverWait(driver, 0).until(EC.presence_of_element_located((By.CLASS_NAME, 'wrap')))
+        # Wait for the content to load (adjust the timeout as needed)
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'wrap')))
 
         # Get the page source after the JavaScript has executed
         page_source = driver.page_source
@@ -57,6 +63,13 @@ try:
         # Increment page number for the next iteration
         page_num += 1
 
+    # Record the end time
+    end_time = time.time()
+
+    # Calculate the time taken
+    elapsed_time = end_time - start_time
+    print(f"Time taken to fetch data: {elapsed_time} seconds")
+
     # Convert the list of dictionaries to a DataFrame
     df = pd.DataFrame(blog_data)
 
@@ -67,6 +80,9 @@ try:
         writer.sheets['Sheet1'].column_dimensions['B'].width = 20
         writer.sheets['Sheet1'].column_dimensions['C'].width = 100
         writer.sheets['Sheet1'].column_dimensions['D'].width = 30
+
+    # Open the Excel file with the default program
+    subprocess.run(['start', 'blog_data.xlsx'], shell=True)
 
 except Exception as e:
     print(f"An error occurred: {e}")
